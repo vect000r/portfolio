dba35e275e7a346af0b1e5e6736b73988cfcfd25
vect0r <vectoreddd@gmail.com>
2025-10-22 21:59:55 +0200

    feat: backend models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Education(models.Model):
+    school = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    years = models.CharField(max_length=100)
+    description = models.TextField()
+    education_id = models.IntegerField()
+
+    class Meta:
+        ordering = ['education_id']
+        verbose_name_plural = 'Education'
+
+    def __str__(self):
+        return self.school
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    url = models.URLField()
+    project_id = models.IntegerField()
+
+    class Meta:
+        ordering = ['project_id']
+
+    def __str__(self):
+        return self.title
+
+
+class Resume(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Resumes'
+
+    def __str__(self):
+        return self.title
