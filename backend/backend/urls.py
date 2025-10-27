83af17d84755318dad110b8e17b6b396c42f8315
vect0r <vectoreddd@gmail.com>
2025-10-28 00:08:01 +0100

    fix: possible admin fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,12 +6,13 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
+
+
