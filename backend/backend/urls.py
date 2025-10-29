b9cecefad19af2f57ec42a22a651b8676bdb52eb
vect0r <vectoreddd@gmail.com>
2025-10-29 17:21:35 +0100

    fix: photos

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,10 +7,12 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 
 
 
