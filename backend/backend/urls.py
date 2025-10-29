759b4ca76c9ec406f51818914e8a2c68f69087f7
vect0r <vectoreddd@gmail.com>
2025-10-29 10:13:43 +0100

    chore: urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,13 +7,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
