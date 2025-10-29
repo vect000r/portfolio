d43359dbd79b3bfb5936e05cc8700cb3a1375b83
vect0r <vectoreddd@gmail.com>
2025-10-29 17:32:38 +0100

    fix: api urls changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .views import (
     EducationViewSet,
@@ -17,6 +15,3 @@ router.register('skill', SkillViewSet, basename='skill')
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
