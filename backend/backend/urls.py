dda714ec102e985a91e06fe1974c85cf044b3e85
vect0r <vectoreddd@gmail.com>
2025-10-29 17:25:52 +0100

    chore: typos

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+]
 
 
 
