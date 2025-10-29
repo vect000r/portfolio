eb635c2274885e6c6eedc220f9d6f70c91f06b2c
vect0r <vectoreddd@gmail.com>
2025-10-29 10:05:00 +0100

    chore: clean urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
