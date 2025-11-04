b52dca4780126125b995922741da8369d596c27c
vect0r <vectoreddd@gmail.com>
2025-11-04 15:35:31 +0100

    fix: temporary urls change

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,6 @@ else:
         }),
     ]
 
-urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
-]
+#urlpatterns += [
+#    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+#]
