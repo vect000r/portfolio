912f86abf837346e67e7c4c747f9374f9ae61327
vect0r <vectoreddd@gmail.com>
2025-12-14 14:37:52 +0100

    fix: urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,5 @@ else:
     ]
 
 urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name='index.html'))
 ]
